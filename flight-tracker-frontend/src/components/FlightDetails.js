078babc6be9dd389e44e96522e8bbc752254a41c
Dylan Jung <Dj5822@gmail.com>
2022-03-26 20:55:03 +1300

    Adjust component positioning
    Adjusted positioning such that it is similar to the mock designs.

--- flight-tracker-frontend/src/components/FlightDetails.js
+++ flight-tracker-frontend/src/components/FlightDetails.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
-import { Button, Card, IconButton, Paper, Typography } from '@mui/material'
+import {
+    Button,
+    Card,
+    Container,
+    IconButton,
+    Paper,
+    Typography,
+} from '@mui/material'
 import plane from '../images/plane-placeholder.jpg'
 import '../styles/FlightDetails.css'
 
@@ -19,38 +26,43 @@ const FlightDetails = () => {
 
     return (
         <div>
-            <Card>
-                <Typography variant="h4">
-                    Plane {flightState.callsign}
-                </Typography>
-                <IconButton color="primary">
-                    <b>X</b>
-                </IconButton>
-                <Paper variant="outlined">
-                    <img className="planeImage" src={plane} alt="a plane" />
-                </Paper>
-
-                <Typography>
-                    <b>Altitude:</b> {flightState.altitude}m
-                </Typography>
-                <Typography>
-                    <b>Vertical Speed:</b> {flightState.vSpeed}m/min
-                </Typography>
-                <Typography>
-                    <b>Speed:</b> {flightState.hSpeed}km/hr
-                </Typography>
-                <Typography>
-                    <b>Heading:</b> {flightState.heading}&deg;
-                </Typography>
-                <Typography>
-                    <b>Distance:</b> {flightState.distance}km
-                </Typography>
-                <Typography>
-                    <b>Squawk:</b> {flightState.squawk}
-                </Typography>
-                <Typography>
-                    <b>Engines:</b> {flightState.engines}
-                </Typography>
+            <Card className="flightDetailsDiv">
+                <div className="flightDetailsRow">
+                    <Typography variant="h4">
+                        Plane {flightState.callsign}
+                    </Typography>
+                    <IconButton color="primary">
+                        <b>X</b>
+                    </IconButton>
+                </div>
+                <div className="flightDetailsRow">
+                    <Paper variant="outlined">
+                        <img src={plane} alt="a plane" className="planeImage" />
+                    </Paper>
+                    <Container>
+                        <Typography>
+                            <b>Altitude:</b> {flightState.altitude}m
+                        </Typography>
+                        <Typography>
+                            <b>Vertical Speed:</b> {flightState.vSpeed}m/min
+                        </Typography>
+                        <Typography>
+                            <b>Speed:</b> {flightState.hSpeed}km/hr
+                        </Typography>
+                        <Typography>
+                            <b>Heading:</b> {flightState.heading}&deg;
+                        </Typography>
+                        <Typography>
+                            <b>Distance:</b> {flightState.distance}km
+                        </Typography>
+                        <Typography>
+                            <b>Squawk:</b> {flightState.squawk}
+                        </Typography>
+                        <Typography>
+                            <b>Engines:</b> {flightState.engines}
+                        </Typography>
+                    </Container>
+                </div>
                 <Button>General</Button>
                 <Button>Spatial</Button>
                 <Button>Speed</Button>
