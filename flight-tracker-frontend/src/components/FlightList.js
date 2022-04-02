0e4f67e18aebbb523f2c5a55b130a859fe378dfa
Murray Freeth-Edwards <murray@freeth.kiwi>
2022-04-03 09:07:02 +1200

    moved flight data api to new function getAllFlights()

--- flight-tracker-frontend/src/components/FlightList.js
+++ flight-tracker-frontend/src/components/FlightList.js
@@ -1,18 +1,5 @@
-// import React from 'react'
+import React from 'react'
 
-async function FlightList(){
-
-    let outData = null;
-    await fetch("https://aircraft.freeth.kiwi/VirtualRadar/AircraftList.json")
-    .then(response => response.json())
-    .then(jsondata => {
-        console.log("data recieved")
-        outData = jsondata.acList;
-    });
-
-    return outData;
-
-}
-
+const FlightList = () => {}
 
 export default FlightList
