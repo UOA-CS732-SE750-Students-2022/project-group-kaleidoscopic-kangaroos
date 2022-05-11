7eb25178b25ed8ea3a6803b94c833982ab10c0d8
Matthew <matt@waiariki.net>
2022-05-12 00:43:39 +1200

    Removed Check for 404 Logic -- Now working.

--- flight-tracker-frontend/src/components/ImageHandler/Airline.js
+++ flight-tracker-frontend/src/components/ImageHandler/Airline.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-console */
-import urlExist from 'url-exist'
-
 // Set the base URL for the airline images.
 const baseURL = 'https://airline.slim.kiwi/logos'
 // Set the default image.
@@ -10,34 +7,10 @@ function getAirlineImage(Callsign, OpIcao) {
     if (OpIcao === undefined) {
         // Check if the we can find the airline from the callsign.
         const slicedCallsign = Callsign.slice(0, 3)
-        const possibleURL = `${baseURL}/${slicedCallsign}.png`
-        console.log('OpIcao', possibleURL)
-        urlExist(possibleURL)
-            .then((vaild) => {
-                if (vaild) {
-                    url = `${possibleURL}`
-                } else {
-                    url = `${baseURL}/PRIVATE.png`
-                }
-            })
-            .catch((err) => {
-                console.log(err)
-            })
+        url = `${baseURL}/${slicedCallsign}.png`
     }
     if (OpIcao !== undefined) {
-        const possibleURL = `${baseURL}/${OpIcao}.png`
-        console.log('Callsign', possibleURL)
-        urlExist(possibleURL)
-            .then((vaild) => {
-                if (vaild) {
-                    url = possibleURL
-                } else {
-                    url = `${baseURL}/UNKNOWN.png`
-                }
-            })
-            .catch((err) => {
-                console.log(err)
-            })
+        url = `${baseURL}/${OpIcao}.png`
     }
     return url
 }
