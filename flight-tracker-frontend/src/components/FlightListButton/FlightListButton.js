207c379db180203c1d430df7608e668fb12facef
David Wagstaff <63911529+dwag351@users.noreply.github.com>
2022-05-13 15:12:57 +1200

    Updated icon files

--- flight-tracker-frontend/src/components/FlightListButton/FlightListButton.js
+++ flight-tracker-frontend/src/components/FlightListButton/FlightListButton.js
@@ -14,7 +14,7 @@ const FlightListButton = ({setVisible}) => {
         <Button variant="contained" color="primary"
             onClick={() => setVisible(true)}
             sx={{
-            position: "fixed", left: 0, top: 120, zIndex:999, padding: 2}}>
+            position: "fixed", left: 0, top: '15%', zIndex:999, padding: 2}}>
             <ImAirplane size={40} /> <Typography variant="h4" sx={{paddingLeft: 2}}>Flight List</Typography> 
         </Button>
     </Slide>
