5214552a8556fa362c9b76eda0de62cc6d3e1314
David Wagstaff <63911529+dwag351@users.noreply.github.com>
2022-05-15 00:17:47 +1200

    Changed menu to icon

--- flight-tracker-frontend/src/components/Settings/Settings.js
+++ flight-tracker-frontend/src/components/Settings/Settings.js
@@ -1,6 +1,6 @@
 import React  from 'react'
 import MenuItem from "@mui/material/MenuItem";
-import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import './Settings.css'
   
@@ -14,29 +14,35 @@ const Settings = () => {
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
-  
-  return (
-    <div className="menuBackground">
-      <Button variant="contained" color="primary"
-        aria-controls="simple-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        Menu
-      </Button>
-      <Menu
-        keepMounted
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        open={Boolean(anchorEl)}
-      >
-        <MenuItem onClick={handleClose}>Flight Information</MenuItem>
-        <MenuItem onClick={handleClose}>About</MenuItem>
-        <MenuItem onClick={handleClose}>Contact</MenuItem>
-        <MenuItem onClick={handleClose}>Settings</MenuItem>
-      </Menu>
-    </div>
-  );
+
+    return (
+      <div className="menuBackground">
+        <IconButton variant="contained" color="primary"
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+          sx={{
+            fontSize: '30px', maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'
+          }}
+        >
+          ⚙️
+        </IconButton>
+        <Menu
+          keepMounted
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          open={Boolean(anchorEl)}
+          sx={{
+            top: '10px', left: '5px'
+          }}
+        >
+          <MenuItem onClick={handleClose}>Flight Information</MenuItem>
+          <MenuItem onClick={handleClose}>About</MenuItem>
+          <MenuItem onClick={handleClose}>Contact</MenuItem>
+          <MenuItem onClick={handleClose}>Settings</MenuItem>
+        </Menu>
+      </div>
+    );
 };
   
 export default Settings;
