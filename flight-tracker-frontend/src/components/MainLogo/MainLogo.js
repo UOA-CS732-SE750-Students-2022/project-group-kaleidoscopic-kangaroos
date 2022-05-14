4289217f2351e662d2e214d74c12654d3b62446c
Dylan Jung <Dj5822@gmail.com>
2022-05-14 23:34:22 +1200

    Update MainLogo documentation

--- flight-tracker-frontend/src/components/MainLogo/MainLogo.js
+++ flight-tracker-frontend/src/components/MainLogo/MainLogo.js
@@ -3,10 +3,7 @@ import MainLogoImage from "../../images/KiwiFlightLogo.png"
 import './MainLogo.css'
 
 /**
- * A button that can be clicked to show the flight list component.
- * @param {boolean} setVisible a function that is used to 
- * make the FlightDetails component visible. 
- * @returns jsx for the button component.
+ * Shows the logo for the application.
  */
 const MainLogo = () => {
     return <img src={MainLogoImage} alt="" className="mainLogo"/>
