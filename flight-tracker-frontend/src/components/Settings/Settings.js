4882a7cd00b0893f6abb364cdcb9b498b2ef0feb
David Wagstaff <63911529+dwag351@users.noreply.github.com>
2022-05-15 01:17:47 +1200

    Update Settings

--- flight-tracker-frontend/src/components/Settings/Settings.js
+++ flight-tracker-frontend/src/components/Settings/Settings.js
@@ -1,8 +1,9 @@
-import React  from 'react'
+import React, { useState } from 'react';
 import MenuItem from "@mui/material/MenuItem";
 import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import './Settings.css'
+import Popup from '../Popup/Popup'
   
 const Settings = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -14,6 +15,12 @@ const Settings = () => {
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
 
     return (
       <div className="menuBackground">
@@ -36,11 +43,18 @@ const Settings = () => {
             top: '10px', left: '5px'
           }}
         >
-          <MenuItem onClick={handleClose}>Flight Information</MenuItem>
-          <MenuItem onClick={handleClose}>About</MenuItem>
-          <MenuItem onClick={handleClose}>Contact</MenuItem>
-          <MenuItem onClick={handleClose}>Settings</MenuItem>
+          <MenuItem onClick={togglePopup}>About</MenuItem>
+        <MenuItem onClick={handleClose}>Settings</MenuItem>
         </Menu>
+        {isOpen && <Popup
+      content={<>
+
+        <b>About</b>
+        <p>Kiwi Flight is a flight tracking company that displays aircraft and flight information in real-time on a map. Kiwi Flight offers flight data such as latitude and longitude positions, origins and destinations, flight numbers, aircraft types, altitudes, headings and speeds.</p>
+
+      </>}
+      handleClose={togglePopup}
+    />}
       </div>
     );
 };
