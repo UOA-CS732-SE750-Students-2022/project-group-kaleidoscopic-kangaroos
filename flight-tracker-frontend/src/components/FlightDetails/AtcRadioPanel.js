10642760ffc30fa9843da3ce8a2869bb797a8729
Dylan Jung <Dj5822@gmail.com>
2022-05-15 00:05:23 +1200

    Update ATC radio

--- flight-tracker-frontend/src/components/FlightDetails/AtcRadioPanel.js
+++ flight-tracker-frontend/src/components/FlightDetails/AtcRadioPanel.js
@@ -1,6 +1,7 @@
 /* eslint-disable arrow-body-style */
 import {
     Container,
+    ListItem,
     Typography,
 } from '@mui/material'
 import PlayAudio from 'react-simple-audio-player'
@@ -9,30 +10,39 @@ import radioChannels from '../../data/atcRadio.json';
 
 // Shows radio information about the plane.
 const AtcRadioPanel = ({details}) => {
+    const radioList = [];
+
+    // Get all the radio channels that are available.
+    radioChannels.forEach((radioChannel) => {
+        if(radioChannel.location_limit_N > details.latitude
+            && radioChannel.location_limit_S < details.latitude
+            && radioChannel.location_limit_E > details.longitude
+            && radioChannel.location_limit_W < details.longitude
+            ){
+            radioList.push(
+                <ListItem key={radioChannel.display_freq} >
+                    <PlayAudio url={radioChannel.stream_url}  />
+                    <Container sx={{
+                    display: "flex",
+                    flexDirection: "column"
+                    }}>
+                        <Typography variant='h6'>
+                        {radioChannel.display_name}
+                        </Typography>
+                        <Typography>
+                            <b>Freq:</b> {radioChannel.display_freq} MHz
+                        </Typography>
+                    </Container>                    
+                </ListItem>
+            );
+        }
+    })
+
+    // List all the radio channels, or if there are no radio channels, show a message.
     return (
         <div className="flightDetailsRow">
             <Container>
-                    {
-                    radioChannels.map((radioChannel) => {
-                        if(radioChannel.location_limit_N > details.latitude
-                            && radioChannel.location_limit_S < details.latitude
-                            && radioChannel.location_limit_E > details.longitude
-                            && radioChannel.location_limit_W < details.longitude
-                            ){
-                            return(
-                                <Container>
-                                <Typography>
-                                    <h3>{radioChannel.display_name}</h3>
-                                    <b>Freq:</b> {radioChannel.display_freq} MHz
-                                </Typography>
-                                <PlayAudio url={radioChannel.stream_url}  />
-                                <hr />
-                                </Container>
-                            );
-                        }
-                        return(<p>No radio channel found</p>);
-                    })
-                    }
+                {radioList.length ? radioList : <p>No radio channels available</p>}
             </Container>
         </div>
     );
