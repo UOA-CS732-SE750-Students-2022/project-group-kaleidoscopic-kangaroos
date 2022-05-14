45edf6f71e9f21a2d7c76bc678e344131a6c3c74
David Wagstaff <63911529+dwag351@users.noreply.github.com>
2022-05-14 20:12:42 +1200

    Removedtests

--- flight-tracker-frontend/src/components/Map/__tests__/Map.test.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-unused-vars */
-import {render} from '@testing-library/react'
-import Map from '../Map';
-
-it('Renders Map component correctly.', () => {
-    const {queryByText} = render(<Map />);
-});
--- flight-tracker-frontend/src/components/Planes/__tests__/Planes.test.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-unused-vars */
-import {render} from '@testing-library/react'
-import Planes from '../Planes';
-
-it('Renders Planes component correctly.', () => {
-    const {queryByText} = render(<Planes />);
-});
