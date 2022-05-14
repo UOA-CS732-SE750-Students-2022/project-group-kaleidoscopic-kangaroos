58d832842d492641a9276dc843ebad59f5078ada
Mett <6138277+elphaa@users.noreply.github.com>
2022-05-15 11:34:04 +1200

    Update Test to reflect Menu

--- flight-tracker-frontend/src/components/Settings/__tests__/Settings.test.js
+++ flight-tracker-frontend/src/components/Settings/__tests__/Settings.test.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-unused-vars */
-import {render} from '@testing-library/react'
-import Settings from '../Settings';
+import { render } from '@testing-library/react'
+import Settings from '../Settings'
 
 it('Renders FlightListButton component correctly.', () => {
-    const {queryByText} = render(<Settings />);
-    expect(queryByText("Flight Information")).toBeTruthy();
-    expect(queryByText("About")).toBeTruthy();
-    expect(queryByText("Contact")).toBeTruthy();
-    expect(queryByText("Settings")).toBeTruthy();
-});
+    const { queryByText } = render(<Settings />)
+    expect(queryByText('About')).toBeTruthy()
+    expect(queryByText('Settings')).toBeTruthy()
+})
